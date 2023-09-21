c471af2461ee36e9bd39c41c649c23fbb8d02909
Charan-1007 <charanrajana10@gmail.com>
2023-09-21 20:30:06 +0530

    buttons and animations are added

---
+++ app/src/main/java/com/example/w_racemk_ii/home/codes/Rsa.kt
@@ -0,0 +1,20 @@
+package com.example.w_racemk_ii.home.codes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.w_racemk_ii.R
+
+
+class Rsa : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rsa, container, false)
+    }
+}
