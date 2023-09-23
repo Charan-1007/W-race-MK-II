4a835fa606a45d9b7d0274c2250348b52ad77887
Charan-1007 <charanrajana10@gmail.com>
2023-09-23 13:55:36 +0530

    About and app icon is updated

--- app/src/main/java/com/example/w_racemk_ii/about/About.kt
+++ app/src/main/java/com/example/w_racemk_ii/about/About.kt
@@ -1,12 +1,15 @@
 package com.example.w_racemk_ii.about
 
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.w_racemk_ii.R
-
 
 class About : Fragment() {
 
@@ -15,7 +18,19 @@ class About : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
 
+        // Get the button reference
+        val githubButton = view.findViewById<Button>(R.id.githubButton)
+
+        // Set the button click listener
+        githubButton.setOnClickListener {
+            // Open the GitHub link in a browser
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Charan-1007/W-race-MK-II.git"))
+            startActivity(intent)
+        }
+
+        return view
+    }
 }
+
